e56f77efc55c569f816a2366ae43123a867e0686
panka <pankajshinde798@gmail.com>
2020-08-26 14:54:29 +0530

    final

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import home
+from .views import home,password
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('password/', password, name='password')
 
 ]
--- password_generator/views.py
+++ password_generator/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+import random
+from .choices import Choices
 
-# Create your views here.
 
 def home(request):
-    context={
-        'password':'pankaj'
+    context = {
+        'Choices': Choices
     }
-    return render(request,'password_generator/home.html',context)
+    return render(request, 'password_generator/home.html', context)
+
+
+def password(request):
+    character = list('abcdefghijklmnopqrstuvwxyz')
+    if 'uppercase' in request.GET:
+        character.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if 'special' in request.GET:
+        character.extend(list('!@#$%^&{}/,.*()'))
+    if 'numbers' in request.GET:
+        character.extend(list('0123456789'))
+    if 'length' in request.GET:
+        length = int(request.GET['length'])
+
+
+    password = ''
+    for _ in range(length):
+        password += random.choice(character)
+
+    return render(request, 'password_generator/password.html', {'password': password})
